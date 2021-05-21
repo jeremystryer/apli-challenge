0d339ecc520275fcabd9b317bce896dee8f0bce8
Jeremy Stryer <jm.stryer@gmail.comgit config --global user.name Jeremy>
2021-05-21 17:18:23 -0500

    Create and insert character div containers and process handlebars template

--- script.js
+++ script.js
@@ -2,11 +2,62 @@ document.addEventListener("DOMContentLoaded", () => {
   let pageUrl = "https://rickandmortyapi.com/api/character";
   let pages;
 
-  fetch(pageUrl)
+  let templates = processTemplates();
+
+  fetch(pageUrl, createCharacterContainer)
     .then(response => response.json())
     .then(data => {
-      pages = data.info.pages; // gives total number of pages
-      pageUrl = data.info.next; // url of next available page
+      // pages = data.info.pages; // gives total number of pages
+      // pageUrl = data.info.next; // url of next available page
       // i can submit another get request for the next url and keep repeating until the page number of that url is equal to the total number of pages
+      console.log(data);
+
+      data.results.forEach(character => {
+        let characterContainer = createCharacterContainer();
+        insertData(character, characterContainer);
+        // console.log(character.image);
+        // console.log(character.name);
+        // console.log(character.status);
+        // console.log(character.species);
+        // console.log(character.gender);
+        // console.log(character.location.name);
+      });
+
     });
+
+    function createCharacterContainer() {
+      let charactersListContainer = document.querySelector("#characters-list-container");
+      let characterContainer = document.createElement("div");
+
+      characterContainer.classList.add("character-container");
+      charactersListContainer.insertAdjacentElement("beforeend", characterContainer);
+      return characterContainer;
+    }
+
+    function insertData(char, charContainer) {
+        // let charData = char;
+        // debugger;
+
+        // charData.forEach(data => {
+          // console.log(data);
+          // charContainer.insert
+
+        let specificCharacter = templates["character-template"](char);
+        charContainer.insertAdjacentHTML("beforeend", specificCharacter);
+        // });
+        // console.log(character.status);
+        // console.log(character.species);
+        // console.log(character.gender);
+        // console.log(character.location.name);
+    }
+
+    function processTemplates() {
+      let templates = {};
+  
+      document.querySelectorAll("script[type='text/x-handlebars']").forEach(tmpl => {
+       templates[tmpl["id"]] = Handlebars.compile(tmpl["innerHTML"]);
+      });
+
+      return templates;
+    }
   }); 
